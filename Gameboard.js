e4e096d84e647d868a5ed97e05c118713e3f2454
raonix <raoni.gillet@gmail.com>
2022-01-12 14:12:01 +0100

    initial commit

---
+++ Gameboard.js
@@ -0,0 +1,41 @@
+export const getEmptyBoard = () => [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0]
+];
+
+const hasValue = (board, value) => {
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[i].length; j++) {
+            if (board[i][j] === value) {
+                return true;
+            }
+        }
+    }
+    return false;
+};
+
+export const isFull = (board) => {
+    return !hasValue(board, 0);
+};
+
+const getRandomPosition = () => {
+    const rowPosition = Math.floor(Math.random() * 4);
+    const colPosition = Math.floor(Math.random() * 4);
+    return [rowPosition, colPosition];
+};
+
+export const generateRandom = (board) => {
+    if (isFull(board)) {
+        return board;
+    }
+    let [row, col] = getRandomPosition();
+    while (board[row][col] !== 0){
+        [row, col] = getRandomPosition();
+    }
+
+    board[row][col] = 2;
+    return board;
+};
+
