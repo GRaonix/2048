fee7fb33b4d277712a54902d10d3021c290c7476
raonix <raoni.gillet@gmail.com>
2022-01-14 09:10:31 +0100

    commit

--- Gameboard.js
+++ Gameboard.js
@@ -52,3 +52,15 @@ const compress = (board) => {
     }
     return newBoard;
 };
+
+const merge = (board) => {
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[i].length - 1; j++) {
+            if (board[i][j] !== 0 && board[i][j] === board[i][j + 1]) {
+                board[i][j] = board[i][j] *2;
+                board[i][j + 1] = 0;
+            }
+        }
+    }
+    return board;
+}
